75a0fd69f018d31f473fb43dfa14abf8754e6e7d
Stefan Meyer <stefan-meyer@sinnerschrader.com>
2012-09-18 10:52:41 +0200

    fixed

--- src/main/java/org/atemsource/atem/impl/pojo/attribute/MapAssociationAttributeFactory.java
+++ src/main/java/org/atemsource/atem/impl/pojo/attribute/MapAssociationAttributeFactory.java
@@ -1,24 +1,14 @@
 /*******************************************************************************
- * Stefan Meyer, 2012
- * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *   http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Stefan Meyer, 2012 Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
  ******************************************************************************/
 package org.atemsource.atem.impl.pojo.attribute;
 
-
 import java.util.Collection;
 import java.util.Map;
-
 import org.atemsource.atem.api.attribute.Attribute;
 import org.atemsource.atem.api.attribute.annotation.MapAssociation;
 import org.atemsource.atem.api.type.PrimitiveType;
@@ -54,15 +44,12 @@ public class MapAssociationAttributeFactory extends AttributeFactory
 		Type targeType = association == null ? null : ctx.getTypeReference(association.targetType());
 		attribute = beanCreator.create(MapAttributeImpl.class);
 
-		if (targeType != null)
-		{
-			PrimitiveType keyType = primitiveTypeFactory.getPrimitiveType(association.keyType());
-			attribute.setTargetType(targeType);
-			attribute.setKeyType(keyType);
-			setStandardProperties(entityType, propertyDescriptor, attribute);
-			attribute.setAccessor(new PojoAccessor(propertyDescriptor.getField(), propertyDescriptor.getReadMethod(),
-				propertyDescriptor.getWriteMethod(), false));
-		}
+		PrimitiveType keyType = association == null ? null : primitiveTypeFactory.getPrimitiveType(association.keyType());
+		attribute.setTargetType(targeType);
+		attribute.setKeyType(keyType);
+		setStandardProperties(entityType, propertyDescriptor, attribute);
+		attribute.setAccessor(new PojoAccessor(propertyDescriptor.getField(), propertyDescriptor.getReadMethod(),
+			propertyDescriptor.getWriteMethod(), false));
 		return attribute;
 
 	}
