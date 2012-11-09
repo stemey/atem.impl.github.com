98b770d5d6b0c83c7105b412742b1f2100f0352b
Stefan Meyer <stefan-meyer@sinnerschrader.com>
2012-11-09 09:41:12 +0100

    changed distribution

--- src/main/java/org/atemsource/atem/impl/common/attribute/primitive/PrimitiveTypeImpl.java
+++ src/main/java/org/atemsource/atem/impl/common/attribute/primitive/PrimitiveTypeImpl.java
@@ -1,46 +1,19 @@
 /*******************************************************************************
- * Stefan Meyer, 2012
- * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *   http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Stefan Meyer, 2012 Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
  ******************************************************************************/
 package org.atemsource.atem.impl.common.attribute.primitive;
 
-
 import java.io.Serializable;
-
 import org.atemsource.atem.api.type.PrimitiveType;
 import org.atemsource.atem.api.type.Type;
 
 
 public abstract class PrimitiveTypeImpl<J> implements PrimitiveType<J>
 {
-
-	
-
-	@Override
-	public boolean isInstance(Object value) {
-		return getJavaType().isInstance(value);
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		return ((PrimitiveType<?>)obj).getJavaType().equals(getJavaType());
-	}
-
-	@Override
-	public int hashCode() {
-		return getJavaType().hashCode();
-	}
 
 	private boolean nullable = true;
 
@@ -52,6 +25,10 @@ public abstract class PrimitiveTypeImpl<J> implements PrimitiveType<J>
 	@Override
 	public J clone(J value)
 	{
+		if (value == null)
+		{
+			return null;
+		}
 		if (value instanceof Number)
 		{
 			return value;
@@ -77,6 +54,18 @@ public abstract class PrimitiveTypeImpl<J> implements PrimitiveType<J>
 	}
 
 	@Override
+	public boolean equals(Object obj)
+	{
+		return ((PrimitiveType<?>) obj).getJavaType().equals(getJavaType());
+	}
+
+	@Override
+	public int hashCode()
+	{
+		return getJavaType().hashCode();
+	}
+
+	@Override
 	public boolean isAssignableFrom(Type<?> source)
 	{
 		return getJavaType().isAssignableFrom(source.getJavaType());
@@ -97,6 +86,12 @@ public abstract class PrimitiveTypeImpl<J> implements PrimitiveType<J>
 		{
 			return a.equals(b);
 		}
+	}
+
+	@Override
+	public boolean isInstance(Object value)
+	{
+		return getJavaType().isInstance(value);
 	}
 
 	@Override
